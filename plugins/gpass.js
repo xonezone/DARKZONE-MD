857b4db939f803c309ed65dc133e2316c9c9e683
✨Darkzone ✨ <135011704+xonezone@users.noreply.github.com>
2025-01-10 02:30:35 +0500

    Update gpass.js

--- plugins/gpass.js
+++ plugins/gpass.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         };
 
         const password = generatePassword(length);
-        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴏʀᴅ ᴍᴀʟᴠɪɴ*`;
+        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴏʀᴅ ᴍoiz*`;
 
         // Send initial notification message
         await conn.sendMessage(from, { text: message }, { quoted: mek });
